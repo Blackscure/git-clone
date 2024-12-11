d932501a63fa620e13b87f0112c53d4af167a18b
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-12-11 16:12:30 +0300

    role url created

--- git_clone/urls.py
+++ git_clone/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('apps/git-clone/api/v1/authentication/', include('authentication.api.urls')),
+    path('apps/git-clone/api/v1/authentication/', include('authentication.api.urls')),
+    path('apps/git-clone/api/v1/role/', include('role.api.urls')),
 ]
+
